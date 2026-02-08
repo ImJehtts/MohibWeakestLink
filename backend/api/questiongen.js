5437a27e0cfe33c05de84dc523e7bc4a8a187d64
Mohib Rehman <rehmanmohib08@gmail.com>
2026-02-08 16:39:32 -0500

    added cors Get

--- backend/api/questiongen.js
+++ backend/api/questiongen.js
@@ -5,6 +5,7 @@ const openai = new OpenAI({
 });
 
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Methods", "GET");
 
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Method not allowed' });
